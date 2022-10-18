9355dd98e05cd9155664488ae9d292cefdf1d05b
Pashalazarev <mymessagebox9@gmail.com>
2022-10-18 19:09:59 +0300

    Initial Commit

---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Pavel Lazarev Macbook on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
